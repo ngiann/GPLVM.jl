0e79397d0719af166906d07159b2c7de0c113dbf
Nikos Gianniotis <ngiann@gmail.com>
2024-02-21 21:31:08 +0100

    added help, comments, type and renamed variable

--- src/woodbury.jl
+++ src/woodbury.jl
@@ -1,15 +1,21 @@
-function woodbury(;K = K, Λ½ = Λ½) 
+"""
+    woodbury(;K = K, Λroot = Λroot)
+
+Calculates (K⁻¹ + Λ)⁻¹ where Λ is diagonal and `Λroot.^2 == Λ`.
+The results should be equivalent to `inv(inv(K)+Diagonal(Λroot).^2)``.
+"""
+function woodbury(;K = K, Λroot::Diagonal{T,Vector{T}} = Λroot) where T
     
     # sources:
-    # see 145 in matrix coobook where we set A⁻¹=K, B=I, C = Λ½
+    # see 145 in matrix coobook where we set A⁻¹=K, B=I, C = Λroot
     # see (A.9) in GPML 
     # The application of the woodbury identity is recommended in GPML, see (3.26) and (3.27).
 
-    KΛ½ = K*Λ½
+    KΛroot = K*Λroot
 
-    U = cholesky(Symmetric(I +  Λ½*KΛ½)).L
+    U = cholesky(Symmetric(I +  Λroot*KΛroot)).L
 
-    A = U\KΛ½'
+    A = U\KΛroot'
 
     Symmetric(K - A'A)
 
