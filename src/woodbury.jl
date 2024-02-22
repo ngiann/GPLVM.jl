8c836477d2a2bf8814e73f1bdf2e1eb8b8f90337
Nikos Gianniotis <ngiann@gmail.com>
2024-02-22 14:24:27 +0100

    updated code, see 3.26-3.28 in GPML

--- src/woodbury.jl
+++ src/woodbury.jl
@@ -1,45 +1,58 @@
 """
-    woodbury_327(;K = K, Λroot = Λroot)
-
-Calculates (K⁻¹ + Λ)⁻¹ where Λ is diagonal and `Λroot.^2 == Λ`.
-The results should be equivalent to `inv(inv(K)+Diagonal(Λroot).^2)``.
+Calculates (K⁻¹ + Λ) where Λ is diagonal and `Λ½.^2 == Λ`.
+Serves as a better mnemonic, for the directly called `woodbury_327` method.
 """
-function woodbury_327(;K = K, Λroot::Diagonal{T,Vector{T}} = Λroot) where T
+aux_invert_K⁻¹_plus_Λ(;K = K, Λroot = Λroot) = woodbury_327(;K = K, W½ = Λroot)
+
+
+"""
+Calculates (K + Λ⁻¹)⁻¹ where Λ is diagonal and `Λ½.^2 == Λ`.
+Serves as a better mnemonic, for the directly called `woodbury_328` method.
+"""
+aux_invert_K_plus_Λ⁻¹(; K = K, Λroot = Λroot) = woodbury_328(;K = K, W½ = Λroot)
+
+
+
+"""
+    woodbury_327(;K = K, W½ = W½)
+
+Calculates (K⁻¹ + W)⁻¹ where W is diagonal and `W½.^2 == W`.
+The results should be equivalent to `inv(inv(K)+Diagonal(W½).^2)``.
+See equations (3.26) and (3.27) in book "Gaussian Processes for Machine Learning" by R&W.
+"""
+function woodbury_327(;K = K, W½::Diagonal{T,Vector{T}} = W½) where T
     
     # sources:
-    # see 145 in matrix coobook where we set A⁻¹=K, B=I, C = Λroot
+    # see 145 in matrix coobook where we set A⁻¹=K, B=I, C = W½
     # see (A.9) in GPML 
     # The application of the woodbury identity is recommended in GPML, see (3.26) and (3.27).
 
-    KΛroot = K*Λroot
+    KW½ = K*W½
 
-    U = cholesky(Symmetric(I +  Λroot*KΛroot)).L
+    B = Symmetric(I +  W½*KW½) # equation (3.26) in GPML.
 
-    A = U\KΛroot'
+    A = Symmetric(KW½ * (B \ KW½'))
 
-    Symmetric(K - A'A)
+    Symmetric(K - A)
 
 end
 
 
-
 """
-    woodbury_328(;K = K, Λroot = Λroot)
+    woodbury_328(;K = K, W½ = W½)
 
-Calculates (K + Λ⁻¹)⁻¹ where Λ is diagonal and `Λroot.^2 == Λ`.
-The results should be equivalent to `inv(K + inv(Diagonal(Λroot).^2))``.
+Calculates (K + W⁻¹)⁻¹ where W is diagonal and `W½.^2 == W`.
+The results should be equivalent to `inv(K + inv(Diagonal(W½).^2))``.
+See equations (3.26) and (3.28) in book "Gaussian Processes for Machine Learning" by R&W.
 """
-function woodbury_328(;K = K, Λroot::Diagonal{T,Vector{T}} = Λroot) where T
+function woodbury_328(; K = K, W½::Diagonal{T,Vector{T}} = W½) where T
     
-    # sources:
-    # The application of the woodbury identity is recommended in GPML, see (3.28).
+    KW½ = K*W½
 
-    KΛroot = K*Λroot
+    B = Symmetric(I +  W½*KW½) # equation (3.26) in GPML
 
-    U = cholesky(Symmetric(I +  Λroot*KΛroot)).L # inside cholesky is matrix B of equation (3.28) in GPML.
+    A = W½*(B\W½) # equation (3.28) in GPML.
 
-    A = U\Λroot'
-
-    Symmetric(A'A)
+    Symmetric(A)
 
 end
