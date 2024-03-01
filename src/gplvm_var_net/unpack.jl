a8fa1760f33602de38c7ca094107708aa686178c
Nikos Gianniotis <ngiann@gmail.com>
2024-03-01 12:12:08 +0100

    remove local and formatting

--- src/gplvm_var_net/unpack.jl
+++ src/gplvm_var_net/unpack.jl
@@ -1,53 +1,49 @@
-#-------------------------------------------
 function unpack(::Val{:gplvmvarnet}, p, ::Missing, D, N, net, Q)
-#-------------------------------------------
 
     nwts = ForwardNeuralNetworks.numweights(net)
 
     @assert(length(p) == Q*N + 2 + 1 + nwts + N)
 
-    local MARK = 0
+    MARK = 0
 
-    local Z = reshape(p[MARK+1:MARK+Q*N], Q, N); MARK += Q*N
+    Z = reshape(p[MARK+1:MARK+Q*N], Q, N); MARK += Q*N
 
-    local θ = exp.(p[MARK+1:MARK+2]); MARK += 2
+    θ = exp.(p[MARK+1:MARK+2]); MARK += 2
 
-    local β = exp(p[MARK+1]); MARK += 1
+    β = exp(p[MARK+1]); MARK += 1
 
-    local w = p[MARK+1:MARK+nwts]; MARK += nwts
+    w = p[MARK+1:MARK+nwts]; MARK += nwts
 
-    local Λroot = Diagonal(reshape(p[MARK+1:MARK+N], N)); MARK += N
+    Λroot = Diagonal(reshape(p[MARK+1:MARK+N], N)); MARK += N
 
     @assert(MARK == length(p))
 
-    local μ = net(w, Z)
+    μ = net(w, Z)
 
     return Z, θ, Fill(β, D, N), μ, Λroot
 
 end
 
 
-#-------------------------------------------
 function unpack(::Val{:gplvmvarnet}, p, 𝛃, D, N, net, Q)
-#-------------------------------------------
 
     nwts = ForwardNeuralNetworks.numweights(net)
 
     @assert(length(p) == Q*N + 2 + nwts + N)
 
-    local MARK = 0
+    MARK = 0
 
-    local Z = reshape(p[MARK+1:MARK+Q*N], Q, N); MARK += Q*N
+    Z = reshape(p[MARK+1:MARK+Q*N], Q, N); MARK += Q*N
 
-    local θ = exp.(p[MARK+1:MARK+2]); MARK += 2
+    θ = exp.(p[MARK+1:MARK+2]); MARK += 2
 
-    local w = p[MARK+1:MARK+nwts]; MARK += nwts
+    w = p[MARK+1:MARK+nwts]; MARK += nwts
 
-    local Λroot = Diagonal(reshape(p[MARK+1:MARK+N], N)); MARK += N
+    Λroot = Diagonal(reshape(p[MARK+1:MARK+N], N)); MARK += N
 
     @assert(MARK == length(p))
 
-    local μ = net(w, Z)
+    μ = net(w, Z)
 
     return Z, θ, 𝛃, μ, Λroot
 
