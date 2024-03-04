b1a8c961fb5b21772ddf2958cc3cd7fb7123f358
Nikos Gianniotis <ngiann@gmail.com>
2024-03-04 09:38:22 +0100

    tidy up

--- src/GPLVM.jl
+++ src/GPLVM.jl
@@ -1,31 +1,56 @@
 module GPLVM
 
-    using LinearAlgebra
-    using Distributions, Random, Statistics
-    using Distances
-    using Optim, PyPlot#, BenchmarkTools#Lux, Functors
-    using Printf, LineSearches, Zygote, ForwardNeuralNetworks
-    using ExponentialExpectations
-
-
-    include("toydata.jl")
-    include("covariance.jl")
-    include("gplvm.jl")
-    # include("gplvmvar_lux.jl")
-    include("gplvmvar_pos.jl")
-    include("gplvmvar.jl")
+    using LinearAlgebra, Distributions, Random, Statistics, Distances
+    using Optim, BlackBoxOptim, Zygote
+    using ExponentialExpectations, FillArrays
+    using ForwardNeuralNetworks
+    using Printf, PyPlot
     
-    include("woodbury.jl")
+    # common
+    include("common/covariance.jl" )
+    include("common/woodbury.jl")
+    include("common/rbf.jl")
+    include("common/myskip.jl")
+    include("common/inverterrors.jl")
+    include("common/toydata.jl")
+    include("common/expectation_latent_function_values.jl")
 
-    include("predict.jl")
-    include("myskip.jl")
-    include("marginallikelihood.jl")
-    include("infertestlatent.jl")
+    export toydata
 
 
-    #---- code for debugging and verifying calculations ----
-    include("marginallikelihood_verify.jl")
+    # standard GPLVM
+    include("gplvm/gplvm.jl")
+    include("gplvm/predictgplvm.jl")
+    include("gplvm/unpack.jl")
+
+    export gplvm
+
+    # variational GPLVM with auxiliary network modelling variational parameters 
+    include("gplvm_var_net/gplvmvarnet.jl")
+    include("gplvm_var_net/marginallikelihood.jl")
+    include("gplvm_var_net/marginallikelihood_VERIFY.jl")
+    include("gplvm_var_net/predictgplvmvarnet.jl")
+    include("gplvm_var_net/numerically_VERIFY.jl")
+    include("gplvm_var_net/unpack.jl")
+
+    # warped GPLVM
+    include("warpedgplvm.jl")
+
+    export warpedgplvm
     
-    export toydata, gplvmvar_pos, gplvmvar, gplvm
+    # GPLVM₊
+    include("gplvm_pos/gplvmvar_pos.jl")
+    include("gplvm_pos/predictivesampler.jl")
+    include("gplvm_pos/marginallikelihood.jl")
+    include("gplvm_pos/marginallikelihood_VERIFY.jl")
+    include("gplvm_pos/infertestlatent.jl")
+    include("gplvm_pos/infertestlatent_photo_rbf.jl")
+    include("gplvm_pos/infertestlatent_photo.jl")
+    include("gplvm_pos/numerically_VERIFY.jl")
+    include("gplvm_pos/unpack.jl")
+
+   
+    export gplvmvar_pos,  gplvmvarnet
+    export inferlatent, infertestlatent_photo, inferlatent_photo_rbf, predictivesampler, predictgplvm, predictgplvmvarnet
 
 end
