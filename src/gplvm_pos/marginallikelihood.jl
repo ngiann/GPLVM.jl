2a70693c2ab0b02d6709dd665c5d92c19142d7e5
Nikos Gianniotis <ngiann@gmail.com>
2024-03-04 20:11:07 +0100

    remove local, comments, correct loglikel by number of missing obs

--- src/gplvm_pos/marginallikelihood.jl
+++ src/gplvm_pos/marginallikelihood.jl
@@ -1,36 +1,38 @@
 function marginallikelihood(::Val{:gplvmvarnet_pos}, X, Z, θ, 𝛃, μ, Λroot, w, α, b; JITTER = JITTER, η = η)
 
-    # sort out dimensions
+    # sort out and verify dimensions
 
-    local N = size(Λroot, 1); @assert(size(μ, 2) == size(Z, 2) == size(X, 2) == N)
+    N = size(Λroot, 1); @assert(size(μ, 2) == size(Z, 2) == size(X, 2) == N)
 
-    local D = size(X, 1); @assert(size(μ, 1) == D)
+    D = size(X, 1); @assert(size(μ, 1) == D)
 
     
     # calculate prior and posterior covariance matrices K and Σ
 
-    local D² = pairwise(SqEuclidean(), Z)
+    D² = pairwise(SqEuclidean(), Z)
 
-    local K  = Symmetric(covariance(D², θ) + JITTER*I)
+    K  = Symmetric(covariance(D², θ) + JITTER*I)
 
-    local Σ  = aux_invert_K⁻¹_plus_Λ(;K = K, Λroot = Λroot)+ JITTER*I
+    Σ  = aux_invert_K⁻¹_plus_Λ(;K = K, Λroot = Λroot) + JITTER*I
 
 
     # log-prior contribution - implements equation eq:log_prior_gplvm_pos
 
-    local ℓ = expectation_of_sum_D_log_prior_zero_mean(;K = K, μ = μ, Σ = Σ)
+    ℓ = expectation_of_sum_D_log_prior_zero_mean(;K = K, μ = μ, Σ = Σ) # ❗check what happens with missing observations.
 
 
     # log-likelihood contribution - implements eq:log_likel_gplvm_pos
 
-    local E, V = expectation_latent_function_values(;α = α, b = b, μ = μ, Σ = Σ)
+    E, V = expectation_latent_function_values(;α = α, b = b, μ = μ, Σ = Σ)
 
-    ℓ += - 0.5*D*N*log(2π) + 0.5*sum(log.(𝛃)) - 0.5*sum(𝛃 .* abs2.(myskip.(X .- E))) - 1/2 * sum(𝛃 .* V)
+    countObs = count(x -> ~ismissing(x), X)
+
+    ℓ += - 0.5*countObs*log(2π) + 0.5*sum(myskip.(log.(𝛃))) - 0.5*sum(𝛃 .* abs2.(myskip.(X .- E))) - 1/2 * sum(myskip.(𝛃 .* V))
 
 
     # entropy contribution with constants discarded - implements eq:entropy_gplvm_pos
 
-    ℓ += D*entropy(Σ) # ❗ don't forget to multiply with D❗
+    ℓ += D*entropy(Σ) # note multiplication with D
 
 
     # penalty on latent coordinates - not in latex
