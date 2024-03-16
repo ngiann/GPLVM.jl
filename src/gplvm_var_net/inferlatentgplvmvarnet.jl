b40cea9f4454e3c91646743c5572b6c2486978b1
Nikos Gianniotis <ngiann@gmail.com>
2024-03-16 16:24:05 +0100

    minimise minus likelihood

--- src/gplvm_var_net/inferlatentgplvmvarnet.jl
+++ src/gplvm_var_net/inferlatentgplvmvarnet.jl
@@ -1,18 +1,25 @@
 function inferlatentgplvmvarnet(ytest, R; iterations = 1000, repeats = 10) 
 
-
     Q = length(R[:Z][:,1]) # dimension of latent space
 
+    CountObs = length(ytest) - count(ismissing, ytest)
 
     function loss(x)
 
-        # for the purpose of inferring the latent coordinate
-        # it suffices to minimise dicrepancy from mean prediction
+        local μpred, Σpred = predictgplvmvarnet(reshape(x,Q,1), R)
 
-        local μpred, = predictgplvmvarnet(reshape(x,Q,1), R)
+        -0.5*CountObs*log(2π) - 0.5*myskip.(sum(abs2.(((ytest - vec(μpred))))))/only(Σpred) - 0.5*CountObs*log(only(Σpred))
 
-        -0.5*sum(abs2.(vec(μpred) - vec(ytest)))
+        # code below implements line above - keep for verification
 
+        # ℓ = zero(eltype(x))
+        # for (m,y) in zip(μpred,ytest)
+        #     if ~ismissing(y)
+        #         ℓ += logpdf(Normal(m, sqrt(only(Σpred))), y)
+        #     end  
+        # end
+        # ℓ
+        
     end
 
     
@@ -21,7 +28,7 @@ function inferlatentgplvmvarnet(ytest, R; iterations = 1000, repeats = 10)
     objective(p) = -loss(p)
 
     function fg!(F, G, x)
-            
+
         value, ∇f = Zygote.withgradient(objective,x)
 
         isnothing(G) || copyto!(G, ∇f[1])
@@ -34,12 +41,12 @@ function inferlatentgplvmvarnet(ytest, R; iterations = 1000, repeats = 10)
 
 
     function getsolution()
-
+        
         luckyindex = ceil(Int, rand() * size(R[:Z],2)) # pick a random coordinate as starting point for optimisation
+     
+        init = optimize(objective, R[:Z][:,luckyindex], NelderMead(), opt).minimizer
 
-        gradloss!(s,p) = copyto!(s, Zygote.gradient(x->-loss(x),p)[1])
-
-        optimize(Optim.only_fg!(fg!), R[:Z][:,luckyindex], ConjugateGradient(), opt)
+        optimize(objective, init, ConjugateGradient(), opt, autodiff=:forward)
 
     end
 
